ef0a83b3bacb971bbf407568ef32f03b3117c6c8
DIEGO CRUZ PATIÑO <143657851+DiegotsCodeHub@users.noreply.github.com>
2025-03-30 21:33:48 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ProyectoTAP___Carreritas_de_F1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Iniciar = new System.Windows.Forms.Button();
+            this.btn_Pausa = new System.Windows.Forms.Button();
+            this.btn_Reanudar = new System.Windows.Forms.Button();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.ptb4 = new System.Windows.Forms.PictureBox();
+            this.ptb3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.ptb2 = new System.Windows.Forms.PictureBox();
+            this.ptb1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Iniciar
+            // 
+            this.btn_Iniciar.Location = new System.Drawing.Point(215, 659);
+            this.btn_Iniciar.Name = "btn_Iniciar";
+            this.btn_Iniciar.Size = new System.Drawing.Size(177, 69);
+            this.btn_Iniciar.TabIndex = 3;
+            this.btn_Iniciar.Text = "Iniciar Carrera";
+            this.btn_Iniciar.UseVisualStyleBackColor = true;
+            this.btn_Iniciar.Click += new System.EventHandler(this.btn_Iniciar_Click);
+            // 
+            // btn_Pausa
+            // 
+            this.btn_Pausa.Location = new System.Drawing.Point(398, 659);
+            this.btn_Pausa.Name = "btn_Pausa";
+            this.btn_Pausa.Size = new System.Drawing.Size(177, 69);
+            this.btn_Pausa.TabIndex = 4;
+            this.btn_Pausa.Text = "Pausar Carrera";
+            this.btn_Pausa.UseVisualStyleBackColor = true;
+            this.btn_Pausa.Click += new System.EventHandler(this.btn_Pausa_Click);
+            // 
+            // btn_Reanudar
+            // 
+            this.btn_Reanudar.Location = new System.Drawing.Point(581, 659);
+            this.btn_Reanudar.Name = "btn_Reanudar";
+            this.btn_Reanudar.Size = new System.Drawing.Size(177, 69);
+            this.btn_Reanudar.TabIndex = 5;
+            this.btn_Reanudar.Text = "Reanudar Carrera";
+            this.btn_Reanudar.UseVisualStyleBackColor = true;
+            this.btn_Reanudar.Click += new System.EventHandler(this.btn_Reanudar_Click);
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.Location = new System.Drawing.Point(764, 659);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(177, 69);
+            this.btn_Restart.TabIndex = 6;
+            this.btn_Restart.Text = "Reiniciar Carrera";
+            this.btn_Restart.UseVisualStyleBackColor = true;
+            this.btn_Restart.Click += new System.EventHandler(this.btn_Restart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // ptb4
+            // 
+            this.ptb4.BackColor = System.Drawing.Color.Transparent;
+            this.ptb4.Image = global::ProyectoTAP___Carreritas_de_F1.Properties.Resources.morado;
+            this.ptb4.Location = new System.Drawing.Point(14, 530);
+            this.ptb4.Name = "ptb4";
+            this.ptb4.Size = new System.Drawing.Size(90, 57);
+            this.ptb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb4.TabIndex = 9;
+            this.ptb4.TabStop = false;
+            // 
+            // ptb3
+            // 
+            this.ptb3.BackColor = System.Drawing.Color.Transparent;
+            this.ptb3.Image = global::ProyectoTAP___Carreritas_de_F1.Properties.Resources.verde;
+            this.ptb3.Location = new System.Drawing.Point(14, 390);
+            this.ptb3.Name = "ptb3";
+            this.ptb3.Size = new System.Drawing.Size(90, 57);
+            this.ptb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb3.TabIndex = 8;
+            this.ptb3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::ProyectoTAP___Carreritas_de_F1.Properties.Resources.pista;
+            this.pictureBox6.Location = new System.Drawing.Point(12, 337);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(1142, 302);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // ptb2
+            // 
+            this.ptb2.BackColor = System.Drawing.Color.Transparent;
+            this.ptb2.Image = ((System.Drawing.Image)(resources.GetObject("ptb2.Image")));
+            this.ptb2.Location = new System.Drawing.Point(14, 225);
+            this.ptb2.Name = "ptb2";
+            this.ptb2.Size = new System.Drawing.Size(90, 57);
+            this.ptb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb2.TabIndex = 2;
+            this.ptb2.TabStop = false;
+            // 
+            // ptb1
+            // 
+            this.ptb1.BackColor = System.Drawing.Color.Transparent;
+            this.ptb1.Image = global::ProyectoTAP___Carreritas_de_F1.Properties.Resources.azul;
+            this.ptb1.Location = new System.Drawing.Point(14, 71);
+            this.ptb1.Name = "ptb1";
+            this.ptb1.Size = new System.Drawing.Size(90, 57);
+            this.ptb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb1.TabIndex = 1;
+            this.ptb1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoTAP___Carreritas_de_F1.Properties.Resources.pista;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1142, 302);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1165, 749);
+            this.Controls.Add(this.ptb4);
+            this.Controls.Add(this.ptb3);
+            this.Controls.Add(this.btn_Restart);
+            this.Controls.Add(this.btn_Reanudar);
+            this.Controls.Add(this.btn_Pausa);
+            this.Controls.Add(this.btn_Iniciar);
+            this.Controls.Add(this.ptb2);
+            this.Controls.Add(this.ptb1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
+            this.Text = "Proyecto Hilos - Topicos Avanzados de Programacion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox ptb1;
+        private System.Windows.Forms.PictureBox ptb2;
+        private System.Windows.Forms.Button btn_Iniciar;
+        private System.Windows.Forms.Button btn_Pausa;
+        private System.Windows.Forms.Button btn_Reanudar;
+        private System.Windows.Forms.Button btn_Restart;
+        private System.Windows.Forms.PictureBox ptb4;
+        private System.Windows.Forms.PictureBox ptb3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ProyectoTAP___Carreritas_de_F1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Hilos para el movimiento de los carros
+        Thread proceso_1;
+        Thread proceso_2;
+        Thread proceso_3;
+        Thread proceso_4;
+
+        // Variables para el movimiento de los carros
+        public const int meta_x = 960;
+        public string mx1 = "derecha";
+        public string mx2 = "derecha";
+        public string mx3 = "derecha";
+        public string mx4 = "derecha";
+        public int x1 = 14, y1 = 71;
+        public int x2 = 14, y2 = 255;
+        public int x3 = 14, y3 = 390;
+        public int x4 = 14, y4 = 530;
+        int a = 0, b = 0, c = 0, d = 0;// Variables de incremento de posición
+        Random ran = new Random();// Generador de números aleatorios
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //Evita que los hilos lancen excepciones al modificar controles de la interfaz
+            CheckForIllegalCrossThreadCalls = false;
+
+            //Agegar sonido de fondo
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = "H:\\Escuela\\Topicos de Programacion\\TopicosdeProgramacion\\Unidad 3\\ProyectoTAP - Carreritas de F1\\Resources\\menu.wav";
+            sp.Play();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            velocidad1();
+            ptb1.Location = new Point(x1, y1); // localizacion de inicio del carro1
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            velocidad2();
+            ptb2.Location = new Point(x2, y2);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            velocidad3();
+            ptb3.Location = new Point(x3, y3);
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            velocidad4();
+            ptb4.Location = new Point(x4, y4);
+        }
+
+        private void btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            // Inicia los timers
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+            timer5.Enabled = true;
+
+            // Iniciar hilos de los carros
+            try
+            {
+                proceso_1 = new Thread(new ThreadStart(Funcion1));
+                proceso_1.Start();
+               
+
+                //Agegar sonido de fondo
+                SoundPlayer sp = new SoundPlayer();
+                sp.SoundLocation = "H:\\Escuela\\Topicos de Programacion\\TopicosdeProgramacion\\Unidad 3\\ProyectoTAP - Carreritas de F1\\Resources\\aceleracion.wav";
+                sp.Play();
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+            try
+            {
+                proceso_2 = new Thread(new ThreadStart(Funcion2));
+                proceso_2.Start();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+            try
+            {
+                proceso_3 = new Thread(new ThreadStart(Funcion3));
+                proceso_3.Start();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+            try
+            {
+                proceso_4 = new Thread(new ThreadStart(Funcion4));
+                proceso_4.Start();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void btn_Pausa_Click(object sender, EventArgs e)
+        {
+            // Pausa los hilos y los timers
+            try
+            {
+                proceso_1.Suspend();
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_2.Suspend();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_3.Suspend();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_4.Suspend();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            timer5.Enabled = false;
+        }
+
+        private void btn_Reanudar_Click(object sender, EventArgs e)
+        {
+            // Reanuda los hilos y los timers
+            try
+            {
+                proceso_1.Resume();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_2.Resume();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_3.Resume();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            try
+            {
+                proceso_4.Resume();
+
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+            timer5.Enabled = true;
+        }
+
+        private void btn_Restart_Click(object sender, EventArgs e)
+        {
+            //Agegar sonido de fondo
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = "H:\\Escuela\\Topicos de Programacion\\TopicosdeProgramacion\\Unidad 3\\ProyectoTAP - Carreritas de F1\\Resources\\alospits.wav";
+            sp.Play();
+
+            // Reinicia las posiciones y los valores de los carros
+            mx1 = "derecha";
+            mx2 = "derecha";
+            mx4 = "derecha";
+            mx4 = "derecha";
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            x1 = 14;
+            y1 = 71;
+            x2 = 14;
+            y2 = 255;
+            x3 = 14;
+            y3 = 390;
+            x4 = 14;
+            y4 = 530;
+            ptb1.Location = new Point(x1, y1);
+            ptb2.Location = new Point(x2, y2);
+            ptb3.Location = new Point(x3, y3);
+            ptb4.Location = new Point(x4, y4);
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            timer5.Enabled = false;
+
+            // Detiene los hilos
+            proceso_1.Abort();
+            proceso_2.Abort();
+            proceso_3.Abort();
+            proceso_4.Abort();
+        }
+
+        // Método para el hilo del carro 1
+        public void Funcion1()
+        {
+            int v1 = ran.Next(5, 50);
+            while (true)
+            {
+                Thread.Sleep(v1);// Simula el movimiento con pausas
+                a++;
+            }
+        }
+
+        public void velocidad1()
+        {
+            if (mx1 == "derecha")
+            {
+                x1 = x1 + a;
+                posicion1();
+            }
+            else
+            {
+            }
+        }
+
+        public void posicion1()
+        {
+            if (x1 >= meta_x)
+            {
+                mx1 = "derecha";
+                x1 = meta_x;
+                timer1.Stop();
+            }
+        }
+
+        // Método para el hilo del carro 2
+        public void Funcion2()
+        {
+            int v2 = ran.Next(5, 50);
+            while (true)
+            {
+                Thread.Sleep(v2);
+                b++;
+            }
+        }
+        public void velocidad2()
+        {
+            if (mx2 == "derecha")
+            {
+                x2 = x2 + b;
+                posicion2();
+            }
+            else
+            {
+            }
+        }
+        public void posicion2()
+        {
+            if (x2 >= meta_x)
+            {
+                mx2 = "derecha";
+                x2 = meta_x;
+                timer2.Stop();
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            ganador();// Verifica el ganador en cada tick
+        }
+
+        // Método para el hilo del carro 3
+        public void Funcion3()
+        {
+            int v3 = ran.Next(5, 50);
+            while (true)
+            {
+                Thread.Sleep(v3);
+                c++;
+            }
+        }
+        public void velocidad3()
+        {
+            if (mx3 == "derecha")
+            {
+                x3 = x3 + c;
+                posicion3();
+            }
+            else
+            {
+            }
+        }
+        public void posicion3()
+        {
+            if (x3 >= meta_x)
+            {
+                mx3 = "derecha";
+                x3 = meta_x;
+                timer3.Stop();
+            }
+        }
+
+        // Método para el hilo del carro 4
+        public void Funcion4()
+        {
+            int v4 = ran.Next(5, 50);
+            while (true)
+            {
+                Thread.Sleep(v4);
+                d++;
+            }
+        }
+        public void velocidad4()
+        {
+            if (mx4 == "derecha")
+            {
+                x4 = x4 + d;
+                posicion4();
+            }
+            else
+            {
+            }
+        }
+        public void posicion4()
+        {
+            if (x4 >= meta_x)
+            {
+                mx4 = "derecha";
+                x4 = meta_x;
+                timer4.Stop();
+            }
+        }
+
+        public void ganador()
+        {
+            int[] pos = { x1, x2, x3, x4 };
+
+            // Determina qué carro está más adelante
+            int max = 0;
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (pos[i] >= pos[max])
+                    max = i;
+            }
+
+            if (pos[max] >= meta_x)
+            {
+                timer5.Enabled = false;
+                MessageBox.Show($"GANADOR: CARRO {max+1}");
+            }
+        }
+    }
+}
